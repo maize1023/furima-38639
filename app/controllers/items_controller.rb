df0ebf48f3635dccff5e9e724607f96cbbd3ef6a
Daiki Iwata <direction2423@gmail.com>
2022-11-19 17:10:19 +0900

    ログイン状態での表示ビューわけ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,7 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sing_in, except: [:index]
+  before_action :move_to_sing_in, except: [:index]
+
   def index
     @items = Item.all
   end
@@ -7,6 +10,8 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+
+  # データ保存したtきはトップページに、失敗したら出品ページに留まる&エラーメッセージ&情報保持
   def create
     Item.create(item_params)
   end
@@ -14,5 +19,11 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id,  :condition_id, :fee_id, :from_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_sing_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,8 @@ class Item < ApplicationRecord
   validates :fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :from_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true
+
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
 
   has_one_attached :image
   belongs_to :user
