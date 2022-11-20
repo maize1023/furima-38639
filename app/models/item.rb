248cc857d7f9a232c0b6247ad4b48c2d46078308
Daiki Iwata <direction2423@gmail.com>
2022-11-20 18:56:29 +0900

    正常系テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,13 +4,13 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :category_id, presence: true,  numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :fee_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :from_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :fee_id, presence: true,       numericality: { other_than: 1 , message: "can't be blank"}
+  validates :from_id, presence: true,      numericality: { other_than: 1 , message: "can't be blank"}
+  validates :day_id, presence: true,       numericality: { other_than: 1 , message: "can't be blank"}
 
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true,        numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
 
   has_one_attached :image
   belongs_to :user
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,70 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+      @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品ができる場合' do
+        it '全ての必須項目が記入されている' do
+          expect(@item).to be_valid
+        end
+    end
+
+
+    # context '商品出品ができない場合' do
+    #   it 'nameが空では登録できない' do
+    #     @item.name = ''
+    #     @item.valid?
+    #     expect(@item.errors.full_messages).to include "Name can't be blank"
+    #   end
+    #   it 'descriptionが空では登録できない' do
+    #     @item.description = ''
+    #     @item.valid?
+    #     expect(@item.errors.full_messages).to include "Description can't be blank"
+    #   end
+    #   it 'category_idが空では登録できない' do
+    #     @item.category_id = ''
+    #     @item.valid?
+    #     expect(@item.errors.full_messages).to include "Category id can't be blank"
+    #   end
+    #   it 'condition_idが空では登録できない' do
+    #     @item.condition_id = ''
+    #     @item.valid?
+    #     expect(@item.errors.full_messages).to include "Condition id can't be blank"
+    #   end
+    #   it 'fee_idが空では登録できない' do
+    #     @item.fee_id = ''
+    #     @item.valid?
+    #     expect(@item.errors.full_messages).to include "Fee id can't be blank"
+    #   end
+    #   it 'from_idが空では登録できない' do
+    #     @item.from_id = ''
+    #     @item.valid?
+    #     expect(@item.errors.full_messages).to include "From id can't be blank"
+    #   end
+    #   it 'day_idが空では登録できない' do
+    #     @item.day_ide = ''
+    #     @item.valid?
+    #     expect(@item.errors.full_messages).to include "Day id can't be blank"
+    #   end
+    #   it 'priceが空では登録できない' do
+    #     @item.price = ''
+    #     @item.valid?
+    #     expect(@item.errors.full_messages).to include "Price can't be blank"
+    #   end
+
+    # end
+
+
+
+
+
+
+
+
+
+
+  end
 end
