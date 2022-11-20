e40be93615ecbef5cb948a4df04a5cc973956365
Daiki Iwata <direction2423@gmail.com>
2022-11-20 19:16:23 +0900

    異常系テストーコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,49 +13,49 @@ RSpec.describe Item, type: :model do
     end
 
 
-    # context '商品出品ができない場合' do
-    #   it 'nameが空では登録できない' do
-    #     @item.name = ''
-    #     @item.valid?
-    #     expect(@item.errors.full_messages).to include "Name can't be blank"
-    #   end
-    #   it 'descriptionが空では登録できない' do
-    #     @item.description = ''
-    #     @item.valid?
-    #     expect(@item.errors.full_messages).to include "Description can't be blank"
-    #   end
-    #   it 'category_idが空では登録できない' do
-    #     @item.category_id = ''
-    #     @item.valid?
-    #     expect(@item.errors.full_messages).to include "Category id can't be blank"
-    #   end
-    #   it 'condition_idが空では登録できない' do
-    #     @item.condition_id = ''
-    #     @item.valid?
-    #     expect(@item.errors.full_messages).to include "Condition id can't be blank"
-    #   end
-    #   it 'fee_idが空では登録できない' do
-    #     @item.fee_id = ''
-    #     @item.valid?
-    #     expect(@item.errors.full_messages).to include "Fee id can't be blank"
-    #   end
-    #   it 'from_idが空では登録できない' do
-    #     @item.from_id = ''
-    #     @item.valid?
-    #     expect(@item.errors.full_messages).to include "From id can't be blank"
-    #   end
-    #   it 'day_idが空では登録できない' do
-    #     @item.day_ide = ''
-    #     @item.valid?
-    #     expect(@item.errors.full_messages).to include "Day id can't be blank"
-    #   end
-    #   it 'priceが空では登録できない' do
-    #     @item.price = ''
-    #     @item.valid?
-    #     expect(@item.errors.full_messages).to include "Price can't be blank"
-    #   end
+    context '商品出品ができない場合' do
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Name can't be blank"
+      end
+      it 'descriptionが空では登録できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Description can't be blank"
+      end
+      it 'category_idが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+      it 'condition_idが空では登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition can't be blank"
+      end
+      it 'fee_idが空では登録できない' do
+        @item.fee_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Fee can't be blank"
+      end
+      it 'from_idが空では登録できない' do
+        @item.from_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "From can't be blank"
+      end
+      it 'day_idが空では登録できない' do
+        @item.day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Day can't be blank"
+      end
+      it 'priceが空では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price can't be blank"
+      end
 
-    # end
+    end
 
 
 
