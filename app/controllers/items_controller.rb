1343efd6ee71b39f80b822fa1011a5cc011ae042
Daiki Iwata <direction2423@gmail.com>
2022-11-22 17:38:14 +0900

    削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,9 +39,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item = Item.find(params[:id])
     if @item.user_id == current_user.id
-      item = Item.find(params[:id])
-      item.destroy
+      @item.destroy
     end
     redirect_to root_path
   end
