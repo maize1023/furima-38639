ba63d9eb6131fe9549c93a54050de5b898ea8e89
Daiki Iwata <direction2423@gmail.com>
2022-11-25 11:47:07 +0900

    カード情報のトークン化

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,24 @@
 const pay = () => {
+  Payjp.setPublicKey("pk_test_d874901967bed2c581720609"); // PAY.JPテスト公開鍵
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("purchase_record_addresse[number]"),
+      cvc: formData.get("purchase_record_addresse[cvc]"),
+      exp_month: formData.get("purchase_record_addresse[exp_month]"),
+      exp_year: `20${formData.get("purchase_record_addresse[exp_year]")}`,
+    };
+
+     Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
