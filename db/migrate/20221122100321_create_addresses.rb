853700d608d68558c3c65147e7c305790d68aa8e
Daiki Iwata <direction2423@gmail.com>
2022-11-27 21:26:19 +0900

    未使用のコード削除

--- db/migrate/20221122100321_create_addresses.rb
+++ db/migrate/20221122100321_create_addresses.rb
@@ -2,7 +2,6 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
 
-      # t.string :name, null: false
       t.string :post_code, null: false
       t.integer :from_id, null: false
       t.string :town, null: false
@@ -10,10 +9,6 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string :building
       t.string :phone, null: false
       t.references :purchase_record, null: false, foreign_key: true
-
-
-
-
       t.timestamps
     end
   end
