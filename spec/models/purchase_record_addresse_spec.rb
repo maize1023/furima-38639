d174054e5b52196e8ce46cb3c2ad0f5271c2c991
Daiki Iwata <direction2423@gmail.com>
2022-11-27 20:14:05 +0900

    テストコードの追加

--- spec/models/purchase_record_addresse_spec.rb
+++ spec/models/purchase_record_addresse_spec.rb
@@ -47,6 +47,36 @@ RSpec.describe PurchaseRecordAddresse, type: :model do
         @purchase_record_addresse.valid?
         expect(@purchase_record_addresse.errors.full_messages).to include("Token can't be blank")
       end
+      it 'user_id(購入者)が空だと保存できないこと' do
+        @purchase_record_addresse.user_id = ''
+        @purchase_record_addresse.valid?
+        expect(@purchase_record_addresse.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_id(購入商品)が空だと保存できないこと' do
+        @purchase_record_addresse.item_id = ''
+        @purchase_record_addresse.valid?
+        expect(@purchase_record_addresse.errors.full_messages).to include("Item can't be blank")
+      end
+      it '郵便番号が半角ハイフンを含む形でなければ購入できない' do
+        @purchase_record_addresse.post_code = '1111111'
+        @purchase_record_addresse.valid?
+        expect(@purchase_record_addresse.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
+      end
+      it '電話番号が9桁以下では購入できない' do
+        @purchase_record_addresse.phone = '111111'
+        @purchase_record_addresse.valid?
+        expect(@purchase_record_addresse.errors.full_messages).to include("Phone is invalid. Include hyphen(-)")
+      end
+      it '電話番号が12桁以上では購入できない' do
+        @purchase_record_addresse.phone = '111111111111111'
+        @purchase_record_addresse.valid?
+        expect(@purchase_record_addresse.errors.full_messages).to include("Phone is invalid. Include hyphen(-)")
+      end
+      it '電話番号に半角数字以外が含まれている場合は購入できない（※半角数字以外が一文字でも含まれていれば良い）' do
+        @purchase_record_addresse.phone = '08011111２11'
+        @purchase_record_addresse.valid?
+        expect(@purchase_record_addresse.errors.full_messages).to include("Phone is invalid. Include hyphen(-)")
+      end
     end
   end
 end
