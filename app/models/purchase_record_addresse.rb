7cfc552b133c7638ff1ef22415836e9f00562b7b
Daiki Iwata <direction2423@gmail.com>
2022-11-27 20:05:19 +0900

    ---のデータが選ばれていないことを確認するバリデーション

--- app/models/purchase_record_addresse.rb
+++ app/models/purchase_record_addresse.rb
@@ -14,7 +14,7 @@ class PurchaseRecordAddresse
     validates :item_id
 
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :from_id
+    validates :from_id, numericality: { other_than: 1 , message: "can't be blank"}
     validates :town
     validates :street
     validates :building
