7873847c4ff71f3ae84dee66e75a65f1d23eb9b0
Daiki Iwata <direction2423@gmail.com>
2022-11-27 21:24:39 +0900

    メモの削除

--- app/models/purchase_record_addresse.rb
+++ app/models/purchase_record_addresse.rb
@@ -1,12 +1,8 @@
 class PurchaseRecordAddresse
-  # attr_accessor :token
 
-  # 保存する前のデータを検証などの処理を実現できるのはActive Modelという仕組みがあるから。
-  # include ActiveModel::Modelを記載しincludeすることで、モデルのように扱えてここにバリデーションの記載もできます！
   include ActiveModel::Model
 
-  # attr_accessorを用いて、保存したい複数のテーブルのカラム名すべてを扱えるようにする
-  # attr_accessorはメソッド内の記述をメソッド外の時に変更を加えれるようにできるメソッドです！つまりインスタンス変数として扱えるようになり、ゲッターとセッターの内部処理を行っています！
+
   attr_accessor :user_id, :item_id, :post_code, :from_id, :town, :street, :building, :phone, :purchase_record_id, :token
 
   with_options presence: true do
@@ -19,13 +15,11 @@ class PurchaseRecordAddresse
     validates :street
     # validates :building
     validates :phone, format: {with: /\A\d{10,11}\z/, message: "is invalid. Include hyphen(-)"}
-    # saveの前に行う
-    # validates :purchase_record_id
+
     validates :token
   end
 
   def save
-    # 各テーブルにデータを保存する処理を書く
     purchase = PurchaseRecord.create(user_id: user_id, item_id: item_id)
     Addresse.create(post_code: post_code, from_id: from_id, town: town, street: street, building: building, phone: phone, purchase_record_id: purchase.id)
   end
