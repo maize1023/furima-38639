ba213f302be34dae49a7661a13820dcbf0104485
Daiki Iwata <direction2423@gmail.com>
2022-11-29 12:32:30 +0900

    パスの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items, only: [:update]
+  resources :items, only: [:index, :new, :create, :destroy, :edit, :update, :show]
   resources :items do
     resources :purchase_records, only: [:create, :index]
   end
